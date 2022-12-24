42538a88a1c89496d6d8b8e83d8d35653232f78e
Gerardo Miranda <gerardodev25@gmail.com>
2022-12-24 10:23:00 -0400

    004 Herencia - Problemática

--- src/clean-code/06-clases-a.ts
+++ src/clean-code/06-clases-a.ts
@@ -1,10 +1,40 @@
-(() => {
-  type Gender = 'M' | 'F';
+type Gender = 'M' | 'F';
 
-  class Person {
-    constructor(public name: string, public gender: Gender, public birtdate: Date) {}
+class Person {
+  constructor(public name: string, public gender: Gender, public birtdate: Date) {}
+}
+
+class User extends Person {
+  public lastAccess: Date = new Date();
+
+  constructor(
+    public email: string,
+    public role: string,
+    public name: string,
+    public gender: Gender,
+    public birtdate: Date
+  ) {
+    super('Gerardo', 'M', new Date());
   }
-  const person = new Person('Gerardo', 'M', new Date());
 
-  console.log(person);
-})();
+  checkCredentials() {
+    return this.role;
+  }
+}
+
+class UserSettings extends User {
+  constructor(
+    public workingDirectory: string,
+    public lastOpenFolder: string,
+    public email: string,
+    public role: string,
+    public name: string,
+    public gender: Gender,
+    public birtdate: Date
+  ) {
+    super(email, role, 'Gerardo', 'M', new Date());
+  }
+}
+
+// const person = new Person('Gerardo', 'M', new Date());
+// console.log(person);
